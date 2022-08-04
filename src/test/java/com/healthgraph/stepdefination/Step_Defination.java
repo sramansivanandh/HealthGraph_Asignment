3c8a0da0c3148de4dd3a623c3c251e1e51dc08ec
sramansivanandh <102310246+sramansivanandh@users.noreply.github.com>
2022-08-04 16:23:28 +0530

    Update Step_Defination.java

--- src/test/java/com/healthgraph/stepdefination/Step_Defination.java
+++ src/test/java/com/healthgraph/stepdefination/Step_Defination.java
@@ -19,7 +19,7 @@ import cucumber.api.java.en.When;
 public class Step_Defination {
 		
 	
-     public static WebDriver driver = Test_Runner.driver;
+     public static WebDriver driver1 = Test_Runner.driver;
 	 Robot r;
 	
 	
@@ -27,15 +27,15 @@ public class Step_Defination {
 	@Given("^user Launch the Application$")
 	public void user_Launch_the_Application() throws Throwable {
 		
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver1.manage().window().maximize();
+		driver1.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		
 	}
 
 	@When("^user Buying the Desired Property$")
 	public void user_Buying_the_Desired_Property() throws Throwable {
 		
-	    WebElement buybutton = driver.findElement(By.xpath("(//div[@class='nb__17yFj'])[1]"));
+	    WebElement buybutton = driver1.findElement(By.xpath("(//div[@class='nb__17yFj'])[1]"));
 	    buybutton.click();
 	    
 	}
@@ -43,7 +43,7 @@ public class Step_Defination {
 	@When("^user Select the City$")
 	public void user_Select_the_City() throws Throwable {
 		
-		WebElement city = driver.findElement(By.xpath("//div[@class='css-1hwfws3 nb-select__value-container nb-select__value-container--has-value']"));
+		WebElement city = driver1.findElement(By.xpath("//div[@class='css-1hwfws3 nb-select__value-container nb-select__value-container--has-value']"));
 		city.click();
 		
 		 r = new Robot();
@@ -57,7 +57,7 @@ public class Step_Defination {
 	@When("^user Enter Locality in Search Box$")
 	public void user_Enter_Locality_in_Search_Box() throws Throwable {
 		
-		WebElement searchbox = driver.findElement(By.xpath("//input[@id='listPageSearchLocality']"));
+		WebElement searchbox = driver1.findElement(By.xpath("//input[@id='listPageSearchLocality']"));
 		searchbox.sendKeys("malad");
 		
 		 r.keyPress(KeyEvent.VK_DOWN);
@@ -87,7 +87,7 @@ public class Step_Defination {
 	@When("^user Select the Apartment Types$")
 	public void user_Select_the_Apartment_Types() throws Throwable {
 		
-		WebElement apartmentType = driver.findElement(By.xpath("(//div[@class='css-v2nw5i-control nb-select__control'])[2]"));
+		WebElement apartmentType = driver1.findElement(By.xpath("(//div[@class='css-v2nw5i-control nb-select__control'])[2]"));
 		apartmentType.click();
 		
 		 r.keyPress(KeyEvent.VK_DOWN);
@@ -111,10 +111,10 @@ public class Step_Defination {
 	@When("^user Click Search button and Navigates To Property Listing Page$")
 	public void user_Click_Search_button_and_Navigates_To_Property_Listing_Page() throws Throwable {
 		
-		WebElement searchbutton = driver.findElement(By.xpath("//button[@class='prop-search-button btn btn-default btn-lg btn-block']"));
+		WebElement searchbutton = driver1.findElement(By.xpath("//button[@class='prop-search-button btn btn-default btn-lg btn-block']"));
 	    searchbutton.click();
 	    
-	    WebElement info = driver.findElement(By.xpath("//div[@class='leave-intent-close-icon']"));
+	    WebElement info = driver1.findElement(By.xpath("//div[@class='leave-intent-close-icon']"));
 	    info.click();
 	    
 	}
@@ -122,9 +122,9 @@ public class Step_Defination {
 	@When("^user ScrollDown the Property Page into Desired Property$")
 	public void user_ScrollDown_the_Property_Page_into_Desired_Property() throws Throwable {
 
-		WebElement property = driver.findElement(By.xpath("(//span[@class='overflow-hidden overflow-ellipsis whitespace-nowrap max-w-80pe po:max-w-full'])[4]"));
+		WebElement property = driver1.findElement(By.xpath("(//span[@class='overflow-hidden overflow-ellipsis whitespace-nowrap max-w-80pe po:max-w-full'])[4]"));
 		
-		JavascriptExecutor js = (JavascriptExecutor) driver;
+		JavascriptExecutor js = (JavascriptExecutor) driver1;
 		js.executeScript("arguments[0].scrollIntoView();", property);
 		
 	}
@@ -132,7 +132,7 @@ public class Step_Defination {
 	@When("^user Click The Desired Property and Navigates To Property Description Page$")
 	public void user_Click_The_Desired_Property_and_Navigates_To_Property_Description_Page() throws Throwable {
 	
-		WebElement property = driver.findElement(By.xpath("(//span[@class='overflow-hidden overflow-ellipsis whitespace-nowrap max-w-80pe po:max-w-full'])[4]"));
+		WebElement property = driver1.findElement(By.xpath("(//span[@class='overflow-hidden overflow-ellipsis whitespace-nowrap max-w-80pe po:max-w-full'])[4]"));
 		property.click();
 		
 	    
@@ -141,8 +141,8 @@ public class Step_Defination {
 	@When("^user ScrollDown the Selected Property Page into Description Section$")
 	public void user_ScrollDown_the_Selected_Property_Page_into_Description_Section() throws Throwable {
 		
-		WebElement description = driver.findElement(By.xpath("(//div[@class='nb__zmA_z nb__9LNOV'])[1]"));
-		JavascriptExecutor js = (JavascriptExecutor) driver;
+		WebElement description = driver1.findElement(By.xpath("(//div[@class='nb__zmA_z nb__9LNOV'])[1]"));
+		JavascriptExecutor js = (JavascriptExecutor) driver1;
 		js.executeScript("arguments[0].scrollIntoView();", description);
 	   
 	}
@@ -150,13 +150,13 @@ public class Step_Defination {
 	@Then("^user Confirm Whether The Description Present Or Not$")
 	public void user_Confirm_Whether_The_Description_Present_Or_Not() throws Throwable {
 		
-		WebElement description = driver.findElement(By.xpath("(//div[@class='nb__zmA_z nb__9LNOV'])[1]"));
+		WebElement description = driver1.findElement(By.xpath("(//div[@class='nb__zmA_z nb__9LNOV'])[1]"));
 		String text = description.getText();
 		System.out.println(text);
 		
 		if (text.equalsIgnoreCase("Description")) {
 			
-			WebElement descriptionContent = driver.findElement(By.xpath("//div[@id='description']"));
+			WebElement descriptionContent = driver1.findElement(By.xpath("//div[@id='description']"));
 			System.out.println("Description available, as shown in below");
 			String text2 = descriptionContent.getText();
 			System.out.println(text2);
@@ -172,7 +172,7 @@ public class Step_Defination {
 	@Then("^user Validating the TestCases$")
 	public void user_Validating_the_TestCases() throws Throwable {
 		
-		WebElement descriptionContent = driver.findElement(By.xpath("//div[@id='description']"));
+		WebElement descriptionContent = driver1.findElement(By.xpath("//div[@id='description']"));
 		
 		Assert.assertNotNull(descriptionContent);
 		System.out.println("Description available, as shown in below");
